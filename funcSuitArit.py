f04fbfb8a148ee99fbaa64d265b78ddeeafe5e1c
YounesChefou <younes.chefou@gmail.com>
2024-01-11 20:43:49 +0100

    test suite arithmetique with function implem

--- funcSuitArit.py
+++ funcSuitArit.py
@@ -1,2 +1,10 @@
 def suit_arithm(sequence):
-	pass
+    if len(sequence) < 2:
+        return True 
+    common_difference = sequence[1] - sequence[0]
+
+    for i in range(2, len(sequence)):
+        if sequence[i] - sequence[i - 1] != common_difference:
+            return False
+
+    return True
