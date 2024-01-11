3cbe1d63b8af61a39649d3c55dfacf47413475c8
YounesChefou <younes.chefou@gmail.com>
2024-01-11 20:36:25 +0100

    test nb prmier

---
+++ test_nbPremier.py
@@ -0,0 +1,19 @@
+
+import unittest
+import nbPremier
+class TestFuncs(unittest.TestCase):
+
+    def test_is_prime(self):
+        self.assertTrue(nbPremier.is_prime(2))      
+        self.assertTrue(nbPremier.is_prime(3))      
+        self.assertTrue(EnbPremier.is_prime(5))      
+        self.assertFalse(nbPremier.is_prime(4))     
+        self.assertFalse(nbPremier.is_prime(9))     
+        self.assertTrue(nbPremier.is_prime(11))         
+        self.assertFalse(nbPremier.is_prime(-7))   
+        self.assertTrue(nbPremier.is_prime(13))  
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
