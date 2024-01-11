ad81397b74dcb4ae53fa1c9904a32b0745e2b8af
YounesChefou <younes.chefou@gmail.com>
2024-01-11 20:50:31 +0100

    test suit geometrique with function implem

--- funcSuiteGeo.py
+++ funcSuiteGeo.py
@@ -1,3 +1,13 @@
 
 def suite_geo(sequence):
-	pass
+    if len(sequence) < 2:
+        return True  
+
+    common_ratio = sequence[1] / sequence[0]
+
+    for i in range(2, len(sequence)):
+        if sequence[i] / sequence[i - 1] != common_ratio:
+            return False
+
+    return True
+
